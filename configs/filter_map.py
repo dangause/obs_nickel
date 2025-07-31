65bc9cb22f13297092e54e8e8a3a9ad8f8cb6ac2
danpgause <danpgause@gmail.com>
2025-07-30 17:16:03 -0700

    updates

---
+++ configs/filter_map.py
@@ -0,0 +1,9 @@
+for source, target in [
+    ("b", "g"),
+    ("v", "g"),
+    ("r", "r"),
+    ("i", "i"),
+    ("u", "g"),
+    ("clear", "r"),
+]:
+    config.filterMap[source] = target
--- python/lsst/obs/nickel/nickelFilters.py
+++ python/lsst/obs/nickel/nickelFilters.py
@@ -1,8 +1,8 @@
 from lsst.obs.base import FilterDefinition, FilterDefinitionCollection
 
 NICKEL_FILTER_DEFINITIONS = FilterDefinitionCollection(
-    FilterDefinition(physical_filter="B", band="B"),
-    FilterDefinition(physical_filter="V", band="V"),
-    FilterDefinition(physical_filter="R", band="R"),
-    FilterDefinition(physical_filter="I", band="I"),
+    FilterDefinition(physical_filter="B", band="b"),
+    FilterDefinition(physical_filter="V", band="v"),
+    FilterDefinition(physical_filter="R", band="r"),
+    FilterDefinition(physical_filter="I", band="i"),
 )
